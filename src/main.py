3fa89386340d2d71b5703a2ffc7fb67170d7e888
atmandy <andylau0985@gmail.com>
2025-12-14 17:30:32 -0800

    generate content

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from gen_content import generate_page, generate_pages_recursive
+from static_copy import static_copy
+
+source_path = "./static"
+dest_path = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
-    res = TextNode("this is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(res)
+    if os.path.exists(dest_path):
+        print("Deleting public dir")
+        shutil.rmtree(dest_path)
 
+    print("Copying...")
+    static_copy(source_path, dest_path)
+
+    print("Generating page...")
+    generate_pages_recursive(dir_path_content, template_path, dest_path)
 
 if __name__ == "__main__":
     main()
